0d65701d4dc07632c6df8730149227ee4594b208
mo-kamal123 <mk4086219@gmail.com>
2026-07-29 13:01:14 +0300

    last edits

--- src/shared/components/Form-Input.jsx
+++ src/shared/components/Form-Input.jsx
@@ -1,19 +1,21 @@
+import { useTranslation } from "react-i18next";
+
 const FormInput = ({
     label,
     type = "text",
     placeholder,
     ...props
   }) => {
+    const { i18n } = useTranslation();
     return (
       <div className="flex flex-col gap-2">
-        <label className="text-sm font-medium text-gray-900">
-          {label}
-        </label>
-  
+        <label className="text-sm font-medium text-gray-900">{label}</label>
+
         <input
+          dir={i18n.language === "ar" ? "rtl" : "ltr"}
           type={type}
           placeholder={placeholder}
-          className="w-full rounded-xl border bg-white border-gray-200 px-4 py-3 outline-none focus:border-main"
+          className="w-full rounded-xl border border-gray-200 bg-white px-4 py-3 outline-none focus:border-main"
           {...props}
         />
       </div>
